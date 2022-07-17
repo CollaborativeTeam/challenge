96afda3354d7677e1e80285d4983cec50b1bf81b
genaroibc <bonavitaismael@gmail.com>
2022-07-17 14:27:07 -0300

    implementing antdesign Table component

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
-import { Table } from '../../shared/Table'
+// import { Table } from '../../shared/Table'
+import { Table } from 'antd'
+import 'antd/dist/antd.css'
+
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
@@ -9,21 +12,25 @@ const Wrapper = styled.div`
 // eslint-disable-next-line no-unused-vars
 const Transactions = [
   {
+    key: 1,
     id: 1,
     value: '1.11',
     from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
   },
   {
+    key: 2,
     id: 2,
     value: '2.22',
     from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
   },
   {
+    key: 3,
     id: 3,
     value: '3.33',
     from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
   },
   {
+    key: 4,
     id: 4,
     value: '4.44',
     from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
@@ -35,11 +42,28 @@ const Configuration = {
   totalOfKeys: Object.keys(Transactions[0]).length,
 }
 
-Transactions.keys()
+const columns = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Value',
+    dataIndex: 'value',
+    key: 'value',
+  },
+  {
+    title: 'From',
+    dataIndex: 'from',
+    key: 'from',
+  },
+]
 const Home = () => {
   return (
     <Wrapper>
-      <Table data={Transactions} config={Configuration} />
+      <Table dataSource={Transactions} columns={columns} />
+      {/* <Table data={Transactions} config={Configuration} /> */}
     </Wrapper>
   )
 }

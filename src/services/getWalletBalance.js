29494472aa3224eb06bf85693d48bf1fb0e053f5
genaroibc <bonavitaismael@gmail.com>
2022-08-28 13:16:03 -0300

    refactor: return promise instead resolved data/error

--- src/services/getWalletBalance.js
+++ src/services/getWalletBalance.js
@@ -1,23 +1,7 @@
 import axios from 'axios'
 
-export async function getWalletBalance(address, params) {
+export function getWalletBalance(address, config) {
   const URL = `${process.env.NEXT_PUBLIC_BASE_URL}1/address/${address}/transactions_v2/`
-
-  try {
-    const response = await axios.get(URL, { params })
-    const result = response.data
-
-    if (result.error) {
-      const { error_code, error_message, error } = result
-      throw {
-        error,
-        error_code,
-        error_message: error_message || 'There was an error',
-      }
-    }
-
-    return result
-  } catch (err) {
-    return err
-  }
+  const { params } = config
+  return axios.get(URL, { params })
 }

5dcad4c320dd99f6d518aa7da02a805dc393428f
genaroibc <bonavitaismael@gmail.com>
2022-09-19 17:51:46 -0300

    feat: implement navigation to transaction tab

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { v4 as uuid } from 'uuid'
-import { getWalletBalance } from 'services/getWalletBalance'
+import { getAddressTransactions } from 'services/getAddressTransactions'
 import { STWrapper } from 'components/shared/styled'
 import { SearchForm } from 'components/dashboard/SearchForm'
 import getSortingFunction from 'helpers/getSortingFunction'
 import { Table } from 'antd'
 import { Message } from 'components/shared/Message'
+import { useTransactionContext } from 'context/TransactionContext'
+import { useRouter } from 'next/router'
 
 const inputValue = 'address'
 const initialParams = {
@@ -23,21 +25,23 @@ export default function Dashboard({}) {
   const [requestError, setRequestError] = useState(null)
   const [loading, setLoading] = useState(false)
 
+  const router = useRouter()
+
+  const { setTransactionData } = useTransactionContext()
+
   const getAddressData = async (address, params) => {
     setLoading(true)
     try {
-      const { data: requestData } = await getWalletBalance(address, {
+      const { data: requestData } = await getAddressTransactions(address, {
         params: { ...initialParams, ...params },
       })
 
       const { items } = requestData.data
 
       if (requestData.error) {
-        const { error_code, error_message } = response.data
-
         throw {
-          code: error_code,
-          message: error_message || 'There was an error',
+          code: requestData.error_code,
+          message: requestData.error_message || 'There was an error',
         }
       }
 
@@ -45,12 +49,12 @@ export default function Dashboard({}) {
         const { block_height, block_signed_at, gas_price, tx_hash } = el
 
         return {
-          // timestamp: Date.parse(block_signed_at),
           creation_date: new Date(block_signed_at).toLocaleString(),
           block_height,
           gas_price,
           tx_hash,
           key: uuid(),
+          unusedKeys: el,
         }
       })
 
@@ -99,23 +103,30 @@ export default function Dashboard({}) {
             Error {requestError.code}: {requestError.message}. Please try again.
           </Message>
         )}
-        {
-          <Table
-            dataSource={tableData}
-            columns={tableColumns}
-            loading={loading}
-            sticky
-            pagination={{
-              total: totalCount,
-              onChange(page, pageSize) {
-                getAddressData(userAddress, {
-                  'page-number': page,
-                  'page-size': pageSize,
-                })
+
+        <Table
+          dataSource={tableData}
+          columns={tableColumns}
+          loading={loading}
+          sticky
+          pagination={{
+            total: totalCount,
+            onChange(page, pageSize) {
+              getAddressData(userAddress, {
+                'page-number': page,
+                'page-size': pageSize,
+              })
+            },
+          }}
+          onRow={(record) => {
+            return {
+              onClick: () => {
+                setTransactionData(record)
+                router.push('transaction')
               },
-            }}
-          />
-        }
+            }
+          }}
+        />
       </section>
     </STWrapper>
   )

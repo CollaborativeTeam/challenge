173f5c5c52cb420e7d8d5cf881b2bfafeedb21a7
genaroibc <bonavitaismael@gmail.com>
2022-10-28 16:49:45 -0300

    add Material UI:

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,8 @@
-import 'antd/dist/antd.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import 'styles/index.css'
 
 import { AddressProvider } from 'context/AddressContext'

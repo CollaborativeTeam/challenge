4e8aba9dfbf4e146f1e289b8dbe3391b2d9a61e8
genaroibc <bonavitaismael@gmail.com>
2022-11-02 19:05:41 -0300

    feat: refactor components

--- src/components/shared/SearchForm.jsx
+++ src/components/shared/SearchForm.jsx
@@ -1,37 +1,49 @@
-import Search from "@mui/icons-material/Search";
-import FormControl from "@mui/material/FormControl";
-import FormGroup from "@mui/material/FormGroup";
-import FormLabel from "@mui/material/FormLabel";
-import Button from "@mui/material/Button";
-import Input from "@mui/material/Input";
+import Search from '@mui/icons-material/Search'
+import FormControl from '@mui/material/FormControl'
+import FormGroup from '@mui/material/FormGroup'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import { Loader } from './Loader'
 
-export function SearchForm ({ handleSubmit, message, inputName }) {
-	return (
-		<FormControl
-			style={{
-				width: "90%",
-				margin: "1rem auto",
-				padding: "1rem",
-				textAlign: "center"
-			}}
-			component="form"
-			onSubmit={handleSubmit}
-		>
-			<FormGroup style={{ margin: "auto" }}>
-				<FormLabel htmlFor={inputName}>{message}</FormLabel>
-				<Input
-					style={{ marginBottom: "2rem" }}
-					fullWidth
-					required
-					minLength={8}
-					type="text"
-					name={inputName}
-					id={inputName}
-				/>
-				<Button type="submit" variant="contained" startIcon={<Search />}>
-          SEARCH
-				</Button>
-			</FormGroup>
-		</FormControl>
-	);
+export function SearchForm({ handleSubmit, message, inputName, loading }) {
+  return (
+    <FormControl
+      style={{
+        width: '100%',
+        maxWidth: '600px',
+        alignItem: 'center',
+      }}
+      component="form"
+      onSubmit={handleSubmit}
+    >
+      <FormGroup
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          margin: '0',
+          flexWrap: 'nowrap',
+          justifyContent: 'center',
+        }}
+      >
+        <TextField
+          style={{ flexGrow: 1 }}
+          required
+          minLength={8}
+          type="text"
+          name={inputName}
+          id={inputName}
+          placeholder={message}
+          variant="outlined"
+        />
+
+        <Button
+          style={{ borderRadius: '0 5px 5px 0', width: '50px' }}
+          type="submit"
+          variant="contained"
+        >
+          {loading ? <Loader color="#000" /> : <Search />}
+        </Button>
+      </FormGroup>
+    </FormControl>
+  )
 }

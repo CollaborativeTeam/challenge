4b698ee01bb48aad798ee437d1d91fa703bdcfd0
genaroibc <bonavitaismael@gmail.com>
2022-11-03 13:23:43 -0300

    refactor: clean home component

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,14 +1,8 @@
 import Typography from '@mui/material/Typography'
 import PaginationBar from 'components/pages/Home/components/PaginationBar'
-import { Loader } from 'components/shared/Loader'
-import { SearchForm } from 'components/shared/SearchForm'
 import { TransactionsTable } from 'components/shared/TransactionsTable/TransactionsTable'
 import { useTransactionsContext } from 'context/TransactionsContext'
-import { isEmpty } from 'helpers/isEmpty'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { getAddressTransactions } from 'services/getAddressTransactions'
-import { v4 as uuid } from 'uuid'
 
 export default function Home() {
   const router = useRouter()
@@ -22,28 +16,18 @@ export default function Home() {
     setTransactionHash,
   } = useTransactionsContext()
 
-  // useEffect(() => {
-  //   setAddressData({
-  //     userAddress,
-  //     pagination,
-  //     transactionsData: tableRows,
-  //     transactionsColumns: tableHeaders,
-  //   })
-  // }, [tableRows, tableHeaders, userAddress, pagination])
+  const handleOnNext = () => {
+    handleTransactionsRequest(null, {
+      'page-number': pagination.page_number + 1,
+    })
+  }
 
-  // useEffect(() => {
-  //   if (!isEmpty(addressData)) {
-  //     const { transactionsData, transactionsColumns, userAddress, pagination } =
-  //       addressData
+  const handleOnPrevious = () => {
+    handleTransactionsRequest(null, {
+      'page-number': pagination.page_number - 1,
+    })
+  }
 
-  //     setTableRows(transactionsData)
-  //     setUserAddress(userAddress)
-  //     setTableHeaders(transactionsColumns)
-  //     setPagination(pagination)
-  //   }
-  // }, [])
-
-  console.log([transactions])
   return (
     <>
       {requestError && (
@@ -66,16 +50,8 @@ export default function Home() {
         <PaginationBar
           nextPage={pagination.next}
           previousPage={pagination.prev}
-          onPrevious={() =>
-            handleTransactionsRequest(null, {
-              'page-number': pagination.page_number - 1,
-            })
-          }
-          onNext={() =>
-            handleTransactionsRequest(null, {
-              'page-number': pagination.page_number + 1,
-            })
-          }
+          onPrevious={handleOnPrevious}
+          onNext={handleOnNext}
         />
       ) : null}
     </>

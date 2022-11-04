51aa149722cf6ca0d7c6e5b01ad3252d9a303810
genaroibc <bonavitaismael@gmail.com>
2022-11-04 12:02:23 -0300

    feat: add more features to transactions context

--- src/context/TransactionsContext.js
+++ src/context/TransactionsContext.js
@@ -1,7 +1,9 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { getAddressTransactions } from "services/getAddressTransactions";
+import { useRouter } from 'next/router'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { getAddressTransactions } from 'services/getAddressTransactions'
+import { getTransactionData } from 'services/getTransactionData'
 
-const TransactionsContext = createContext();
+const TransactionsContext = createContext()
 
 const INITIAL_PARAMS = {
   key: process.env.NEXT_PUBLIC_API_KEY,
@@ -11,28 +13,37 @@ const INITIAL_PARAMS = {
 }
 
 export const TransactionsProvider = ({ children }) => {
-  const [transactions, setTransactions] = useState([]);
-  const [transaction, setTransaction] = useState([]);
-  const [requestError, setRequestError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [transactionHash, setTransactionHash] = useState('');
-  const [userAddress, setUserAddress] = useState('');
+  const [transactions, setTransactions] = useState([])
+  const [transaction, setTransaction] = useState([])
+  const [requestError, setRequestError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [transactionHash, setTransactionHash] = useState('')
+  const [userAddress, setUserAddress] = useState('')
   const [pagination, setPagination] = useState({
     prev: null,
     next: null,
     page_number: null,
-  });
+  })
+
+  const router = useRouter()
 
   useEffect(() => {
-    setTransaction(transactions.find(transaction => transaction.tx_hash === transactionHash))
-  }, [transactionHash]);
+    setTransaction(
+      transactions.find(
+        (transaction) => transaction.tx_hash === transactionHash
+      ) ?? {}
+    )
+  }, [transactionHash])
 
   const handleTransactionsRequest = async (address, params) => {
     setLoading(true)
     try {
-      const { data: requestData } = await getAddressTransactions(address || userAddress, {
-        params: { ...INITIAL_PARAMS, ...params },
-      })
+      const { data: requestData } = await getAddressTransactions(
+        address || userAddress,
+        {
+          params: { ...INITIAL_PARAMS, ...params },
+        }
+      )
 
       if (requestData.error) {
         throw {
@@ -46,25 +57,38 @@ export const TransactionsProvider = ({ children }) => {
         pagination: { has_more, page_number },
       } = requestData.data
 
-      // const tableRows = items.map((el) => {
-      //   const { block_height, block_signed_at, gas_price, tx_hash } = el
-
-      //   return {
-      //     creation_date: new Date(block_signed_at).toLocaleString(),
-      //     block_height,
-      //     gas_price,
-      //     tx_hash,
-      //     key: uuid(),
-      //     unusedKeys: el,
-      //   }
-      // })
-
       setRequestError(null)
       setTransactions(items)
       setPagination({ prev: page_number > 0, next: has_more, page_number })
     } catch (err) {
       setRequestError(err)
       setTransactions([])
+    } finally {
+      setLoading(false)
+
+      if (!/^\/$/.test(router.pathname)) {
+        router.push('/')
+      }
+    }
+  }
+
+  const handleTransactionRequest = async (txHash) => {
+    setLoading(true)
+    try {
+      const { data: requestData } = await getTransactionData(txHash)
+
+      if (requestData.error) {
+        throw {
+          code: requestData.error_code,
+          message: requestData.error_message || 'There was an error',
+        }
+      }
+
+      setRequestError(null)
+      setTransaction(requestData.data.items[0])
+    } catch (err) {
+      setRequestError(err)
+      setTransaction({})
     } finally {
       setLoading(false)
     }
@@ -88,17 +112,18 @@ export const TransactionsProvider = ({ children }) => {
         transaction,
         setTransaction,
         handleTransactionsRequest,
+        handleTransactionRequest,
       }}
     >
       {children}
     </TransactionsContext.Provider>
-  );
-};
+  )
+}
 
 export const useTransactionsContext = () => {
-  const context = useContext(TransactionsContext);
+  const context = useContext(TransactionsContext)
 
-  if (!context) throw new Error("Transactions context is not defined here!!");
+  if (!context) throw new Error('Transactions context is not defined here!!')
 
-  return context;
-};
+  return context
+}
